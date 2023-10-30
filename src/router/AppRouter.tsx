83732b0da5eec4cde42aa8a253e2fe17dfb1e40f
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-10-30 14:59:36 -0300

    Project setup

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from '../pages/HomePage';
+
+export const AppRouter = () => {
+    return <>
+        <Routes>
+            <Route path='/' element={<HomePage />} />
+
+
+        </Routes>
+    </>
+
+}

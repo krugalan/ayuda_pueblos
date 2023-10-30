1f1d1aaf87be65688de40132be2efff4626da82b
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-10-30 17:00:22 -0300

    upd

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from '../pages/HomePage';
+import { ContactPage } from '../pages/ContactPage';
+import { AboutPage } from '../pages/AboutPage';
 
 export const AppRouter = () => {
     return <>
         <Routes>
             <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/contact' element={<ContactPage />} />
 
 
         </Routes>
